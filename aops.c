31beba0393d5e480d3a7b1e1b150f0f85dfaea36
Andrei Petre <p31andrei@gmail.com>
2014-05-24 12:26:39 +0300

    Add a check to not go beyond max blocks.

--- aops.c
+++ aops.c
@@ -8,8 +8,11 @@ int pitix_get_block(struct inode *inode, sector_t iblock, struct buffer_head *bh
 			struct pitix_inode_info, vfs_inode);
 	struct super_block *sb = inode->i_sb;
 
+	int block = pii->data_blocks[iblock];
+
 	printk(LOG_LEVEL "get_block inode=[%ld] iblock=[%ld]\n", inode->i_ino, iblock);
-	map_bh(bh, inode->i_sb, pitix_sbi(sb)->dzone_block + pii->data_blocks[iblock]);
+	if (block < get_blocks(sb))
+		map_bh(bh, inode->i_sb, pitix_sbi(sb)->dzone_block + block);
 
 	return 0;
 }
